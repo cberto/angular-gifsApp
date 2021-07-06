c22b760e4ae50ee7aa4ddbd9b693a695eb4d395d
cberto <bertolanicami@gmail.com>
2021-07-06 10:27:48 -0300

    Tipado a las peticiones http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  private apiKey: string= 'UyNQYQfX0knI5qMgApdAsscWL7EwJqGr';
   private _historial:string[]=[];
+  //cambiar any por su tipo correspondiente
+  public resultados:Gif[] = [];
 
   get historial(){
 
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient){}
 
   //insertar valoresres
   buscarGifs(query:string =''){
@@ -24,6 +32,15 @@ export class GifsService {
  this._historial =this._historial.splice(0,10);
     }
 
-    console.log(this._historial);
+  //  console.log(this._historial);
+  //tipo generico
+     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=UyNQYQfX0knI5qMgApdAsscWL7EwJqGr&q=${query}&limit=10`)
+     .subscribe((resp) => {
+       console.log(resp.data);
+       this.resultados=resp.data;
+
+     });
+
+     //suscripcion para la resolucion del get
   }
 }
